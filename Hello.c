d0cbe1684cc2113d6d41f55aec942e1e7768aa0c
69011817 วชิระ <69011817@kmitl.ac.th>
2026-09-23 15:05:55 +0700

    .\Hello.c

--- Hello.c
+++ Hello.c
@@ -3,13 +3,12 @@ int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
     return 0;
-}
-// Print a greeting message for the specified user
+} // Print a greeting message for the specified user
 int farewell(const char *name)
 {
 printf("Goodbye, %s!\n", name);
 return 0;
-}
+} // Print a farewell message for the specified user
 int main(void)
 {
     greeting("Developer A,B,C !!");
