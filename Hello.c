d0797af06a20362ca0fe0bdde281e7a3fdac9349
69011817 วชิระ <69011817@kmitl.ac.th>
2026-09-23 14:54:36 +0700

    Add greeting function to print user messages

--- Hello.c
+++ Hello.c
@@ -4,6 +4,7 @@ int greeting(const char *name)
     printf("Hello, %s!\n", name);
     return 0;
 }
+// Print a greeting message for the specified user
 int main(void)
 {
     greeting("Developer A,B,C !!");
